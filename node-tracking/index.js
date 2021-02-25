0153711a31d83bb6638433f450c19e3875db8f91
Diego <diegoantunescosta@gmail.com>
2021-02-25 11:37:15 -0300

    array events backend

--- node-tracking/index.js
+++ node-tracking/index.js
@@ -1,11 +1,18 @@
 const express = require ('express'); 
 const cors = require ('cors');
 const TrackingCorreios = require ('tracking-correios');
-const app =express();
+const app = express();
 app.use (cors());
 
 
 const port = 3001;
+
+const get =(obj, path, fallback = null)=>{
+    const pathKeys = (typeof path === 'string') ? path.split('.'): [];
+    const result = pathKeys.reduce((value,key) => value && value[key], obj);
+    return result || fallback;
+}
+
 
 
 app.get ('/',(req, res) => {
@@ -13,11 +20,14 @@ app.get ('/',(req, res) => {
 
     TrackingCorreios.track(tracking)
         .then ((result) =>{
-        res.json({message:'Encomenda do Opressor encontrada',tracking,result})
+
+        console.log ('evento', get (result,'0.evento'));      
+
+        res.json({message:'Encomenda do Opressor encontrada',tracking,result});
         }) 
         .catch((error)=>{
         res.json({message: 'Codigo de rastreio do Opressor não encontrado !',error});
-        });
+        })
 });
 
-app.listen (port,() => console.log(`${port} connected`));
+app.listen (port,() => console.log(`connected ${port}`))
