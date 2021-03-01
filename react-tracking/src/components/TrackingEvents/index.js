b2eaeea8b8f6a87855892117282fdc39eb87fc0d
Diego <diegoantunescosta@gmail.com>
2021-03-01 11:30:48 -0300

    result in frontend

--- react-tracking/src/components/TrackingEvents/index.js
+++ react-tracking/src/components/TrackingEvents/index.js
@@ -1,8 +1,27 @@
-import React from  'react';
+import React from "react";
 
-function TrackingEvents (){
+function TrackingResult({ data }) {
+  if (!data || !data.length === 0) return null;
 
-    return(<h1> Tracking Events</h1>)
+  return (
+    <>
+      <h1>Tracking Result</h1>
+      <ul className="list-group">
+        {data.map(item => {
+          const { data, origem, destino, status } = item;
+
+          return (
+            <li key={status} className="list-group-item">
+              <span className="badge badge-primary">{origem}</span>
+              <span>data: {data}</span>
+              <span>destino: {destino}</span>
+              <span>status: {status}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
 
-export default TrackingEvents;
+export default TrackingResult;
